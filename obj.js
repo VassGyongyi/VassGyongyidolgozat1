2a5f3fd1a2af718b288e83c543cb5a0592bf6b81
VassGyongyi <s.v.gyongyi@gmail.com>
2023-03-18 10:01:52 +0100

    lista léterehozása

---
+++ obj.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", init);
+const KUTYAK = [
+  {
+    nev: "Dézi",
+    fajta: "keverék",
+    lab: 4,
+    marmagassag: 52,
+    neme: "szuka",
+    kor: 13,
+  },
+  {
+    nev: "Kira",
+    fajta: "terrier",
+    lab: 4,
+    marmagassag: 20,
+    neme: "szuka",
+    kor: 13,
+  },
+];
+console.log(KUTYAK)
+function init() {
+  //kulcs-érték párok
+ 
+  console.log(KUTYAK[0]);
+  console.log(KUTYAK[0].nev);
+  KUTYAK[0].nev = "Béla";
+  console.log(KUTYAK[0].nev);
+  KUTYAK[0].oltas = "van";
+  delete KUTYAK[0].lab;
+  console.log(KUTYAK[0]);
+
+  //összes bejárása
+  for (const key in KUTYAK[0]) {
+    console.log(key, KUTYAK[0][key]);
+  }
+}
--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 window.addEventListener("load", init);
 const kutyaLista = [
-    "foxterrier",
-    "kuvasz",
-    "puli",
-    "agár",
-    "újfullandi",
-    "keverék",
-  ];
+  "foxterrier",
+  "kuvasz",
+  "puli",
+  "agár",
+  "újfullandi",
+  "keverék",
+];
 function init() {
- 
-const ART = document.querySelector("article");
-ART.innerHTML +='<button>Kutya</button>'
-const GOMB =document.querySelectorAll("article button")[0];
-GOMB.addEventListener("click",egerEsemeny);
+  const ART = document.querySelector("article");
+  // ART.innerHTML +='<button>Kutya</button>'
+  // const GOMB =document.querySelectorAll("article button")[0];
+  // GOMB.addEventListener("click",egerEsemeny);
+
+  let txt = osszeallit();
+  ART.innerHTML += txt;
 }
 
-function egerEsemeny(){
-    const ART2 = document.querySelector("article");
-    ART2.innerHTML += `${kutyaLista[2]}`;
+// function egerEsemeny(){
+//     const ART2 = document.querySelector("article");
+//     ART2.innerHTML += `${kutyaLista[2]}`;
+// }
+
+function osszeallit() {
+  //összeállítjuk azt a szöveges tartalmat, ami a HTML kódot jelenti
+  let txt = "";
+  for (let index = 0; index < kutyaLista.length; index++) {
+    txt += `<div><p>${kutyaLista[index]}</p></div>`;
+  }
+  return txt;
 }
